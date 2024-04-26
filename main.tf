6cf01dfb3dc0f7880f3f0c87a8d4e04e36a0057a
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-26 14:51:59 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -21,9 +21,9 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-#resource "aws_route" "igw" {
-#  for_each = lookup(lookup(module.subnets, "public", null), route
-#  route_table_id            = aws_route_table.testing.id
-#  destination_cidr_block    = "10.0.1.0/22"
-#  vpc_peering_connection_id = "pcx-45ff3dc1"
-#}
+resource "aws_route" "igw" {
+  for_each               = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id         = each.value["id"]
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
