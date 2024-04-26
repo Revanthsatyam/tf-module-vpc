0e9ebb2a4153f44f3b2f78439c9ab7a24c9b332d
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-26 15:37:04 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_route" "ngw" {
 }
 
 resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = var.default_vpc_id
+  auto_accept = true
 }
