9fc39a6cbf176c2a300188cb294ea8362465b7cc
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-26 15:58:55 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_route" "default-vpc-peer-entry" {
 
 resource "aws_instance" "main" {
   instance_type = "t3.micro"
-  image_id = "ami-03265a0778a880afb"
+  ami = "ami-03265a0778a880afb"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
