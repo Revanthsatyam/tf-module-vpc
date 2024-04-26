99ab5c0a0e0182050c4a51ab24ec381b665826b8
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-26 12:40:46 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -48,14 +48,20 @@ resource "aws_route" "ngw" {
 }
 
 resource "aws_vpc_peering_connection" "peering" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
-  auto_accept   = true
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = var.default_vpc_id
+  auto_accept = true
 }
 
 resource "aws_route" "peering" {
-  count                  = length(local.private_route_table_ids)
-  route_table_id         = element(local.private_route_table_ids, count.index)
-  destination_cidr_block = var.default_vpc_cidr
-  gateway_id             = aws_vpc_peering_connection.peering.id
+  count                     = length(local.private_route_table_ids)
+  route_table_id            = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "default-vpc-peer-entry" {
+  route_table_id            = var.default_vpc_route_table_id
+  destination_cidr_block    = var.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
