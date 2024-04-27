0c4820520d207e3fef03792de772cc374bcc4097
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-27 07:44:46 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
-
-  tags = {
-    Name = "dev"
-  }
+  tags       = merge(local.tags, { Name = "${var.env}-vpc" })
 }
 
 module "subnets" {
@@ -11,14 +8,13 @@ module "subnets" {
   for_each = var.subnets
   vpc_id   = aws_vpc.main.id
   subnets  = each.value
+  tags     = local.tags
+  env      = var.env
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "dev-igw"
-  }
+  tags   = merge(local.tags, { Name = "${var.env}-igw" })
 }
 
 resource "aws_route" "igw" {
@@ -38,6 +34,7 @@ resource "aws_nat_gateway" "ngw" {
   allocation_id = element(aws_eip.ngw.*.id, count.index)
   subnet_id     = element(local.public_subnet_ids, count.index)
   depends_on    = [aws_internet_gateway.igw]
+  tags          = merge(local.tags, { Name = "${var.env}-ngw" })
 }
 
 resource "aws_route" "ngw" {
@@ -51,6 +48,7 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.default_vpc_id
   auto_accept = true
+  tags        = merge(local.tags, { Name = "${var.env}-peer" })
 }
 
 resource "aws_route" "peer" {
